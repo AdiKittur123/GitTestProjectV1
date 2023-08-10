d234f81c2c93b3bd951d119cc5d614631389c5d0
ADITYA <ADITYA@LAPTOP-G83G994S>
2023-08-11 03:34:10 +0530

    Updating - Employee side

---
+++ src/Git3.java
@@ -0,0 +1,12 @@
+public class Git3
+{
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+				
+				if(hungry == "Yes")
+				{System.out.println("eat somthing..");}
+				else
+				{System.out.println("do work..");}
+	} 
+}
